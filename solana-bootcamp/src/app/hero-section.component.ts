97f1de0e3ce799c35c4d0e63c11f01340421505d
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-02-13 03:12:24 -0300

    Se agregan los componentes Hero y Features
    Actualizo:
    
    solana-bootcamp/src/app/hero-section.component.ts
    solana-bootcamp/src/app/features-section.component.ts
    solana-bootcamp/src/app/home-page.components.ts

---
+++ solana-bootcamp/src/app/hero-section.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'solana-hero-section',
+    template: `Hero`,
+    standalone: true,
+})
+
+export class HeroSectionComponent {}
