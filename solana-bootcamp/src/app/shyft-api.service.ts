e4b9ce7e269d589fcdde8d9a9135374e3533e3d6
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-02-14 17:14:57 -0300

    Se corrigen las dependencias importadas
    Actualizo:
    
    solana-bootcamp/src/app/

--- solana-bootcamp/src/app/shyft-api.service.ts
+++ solana-bootcamp/src/app/shyft-api.service.ts
@@ -28,3 +28,23 @@ export class ShyftApiService {
     }
 }
 
+//getTransactionHistory(publicKey: string | undefined | null) {
+//        if (!publicKey) {
+//            return of(null);
+//        }
+        
+//        const url = new URL('https://api.shyft.to/sol/v1/wallet/token_balance');
+
+//        url.searchParams.set('network', 'mainnet-beta');
+//        url.searchParams.set('wallet', publicKey);
+//        url.searchParams.set('token', this._mint);
+
+//        return this._httpClient
+//        .get<{ 
+//            result: { balance: number; info: { image: string } };
+//    }>(url.toString(), { headers: this._header })
+//    .pipe(map((response) => response.result));
+//    }
+//}
+
+
