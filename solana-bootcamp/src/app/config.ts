f603a45b14dfea06d21b107f4391072e5130ada1
Gabriel  Juan Alejandro Juhasz <88912420+GabJaJ@users.noreply.github.com>
2024-11-13 18:48:08 -0300

    Updating DApp
    Updating DApp

--- solana-bootcamp/src/app/config.ts
+++ solana-bootcamp/src/app/config.ts
@@ -1,4 +1,6 @@
 export const config = {
-        shyftApiKey: 'QobHfFkMqo307X2S',
-        mint: '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs',
+        shyftApiKey: '
+        ',
+        mint: '
+        ',
     };
